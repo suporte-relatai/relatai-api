63b3c2077edba6077efeea21c23740e548016bf9
suporte-relatai <36606797+suporte-relatai@users.noreply.github.com>
2018-02-19 16:35:29 -0300

    Update Usuario.java

--- src/main/java/br/relatai/tcc/dominio/Usuario.java
+++ src/main/java/br/relatai/tcc/dominio/Usuario.java
@@ -1,8 +1,6 @@
 package br.relatai.tcc.dominio;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
-
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -20,8 +18,6 @@ public class Usuario {
 	
 	private LocalDate dataCadastro;
 	
-	private LocalTime horaCadastro;
-	
 	@Id
 	public String getId() {return id;}
 	public void setId(String id) {this.id = id;}
@@ -31,13 +27,7 @@ public class Usuario {
 	public String getCelular() {return celular;}
 	public void setCelular(String celular) {this.celular = celular;}
 			
-	@Field("data_cadastro")
-	@JsonFormat(pattern = "dd/MM/yyyy")
+	@Field("data_cadastro") @JsonFormat(pattern = "dd/MM/yyyy")	
 	public LocalDate getDataCadastro() {return dataCadastro;}
-	public void setDataCadastro(LocalDate dataCadastro) {this.dataCadastro = dataCadastro;}
-	
-	@JsonFormat(pattern = "HH:mm", locale="pt-BR", timezone="America/Sao_Paulo")
-	@Field("hora_cadastro")	
-	public LocalTime getHoraCadastro() {return horaCadastro;}
-	public void setHoraCadastro(LocalTime horaCadastro) {this.horaCadastro = horaCadastro;}	
+	public void setDataCadastro(LocalDate dataCadastro) {this.dataCadastro = dataCadastro;}	
 }
