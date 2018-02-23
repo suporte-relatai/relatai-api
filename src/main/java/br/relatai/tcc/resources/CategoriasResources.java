d227842cad54d77f33be4a759b35b0994ce85eaa
suporte-relatai <36606797+suporte-relatai@users.noreply.github.com>
2018-02-22 23:12:51 -0300

    Add files via upload

--- src/main/java/br/relatai/tcc/resources/CategoriasResources.java
+++ src/main/java/br/relatai/tcc/resources/CategoriasResources.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import br.relatai.tcc.services.CategoriasServices;
 
 @RestController
 @RequestMapping("/categorias")
+@CrossOrigin
 public class CategoriasResources {
 
 	@Autowired
--- src/main/java/br/relatai/tcc/resources/UsuariosResources.java
+++ src/main/java/br/relatai/tcc/resources/UsuariosResources.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import br.relatai.tcc.services.UsuariosServices;
 
 @RestController
 @RequestMapping("/usuarios")
+@CrossOrigin
 public class UsuariosResources {
 
 	@Autowired
